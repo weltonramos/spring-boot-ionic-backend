52f7ec8752bf061e282257fbe71d7356768547df
Welton Ramos <wel035@hotmail.com>
2020-05-14 11:15:18 -0300

    Criacao categorias e insercao classe dominio

--- src/main/java/com/weltonramos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/weltonramos/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,28 @@
 package com.weltonramos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.weltonramos.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "Rest esta funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informática");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 }
