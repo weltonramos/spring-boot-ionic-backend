662be7d42dacec86db2aa9b1f84131f685791d75
Welton Ramos <wel035@hotmail.com>
2020-05-14 12:18:22 -0300

    Mapeamento de classe Categoria e adição de dependencias H2 no POM

--- src/main/java/com/weltonramos/cursomc/domain/Categoria.java
+++ src/main/java/com/weltonramos/cursomc/domain/Categoria.java
@@ -2,15 +2,22 @@ package com.weltonramos.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
 	
 	public Categoria() {
-		
 	}
 	
 	public Categoria(Integer id, String nome) {
@@ -58,6 +65,4 @@ public class Categoria implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
 }
