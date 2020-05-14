23225bdfe47b273d48f5d4203f5d1afe132491aa
Welton Ramos <wel035@hotmail.com>
2020-05-14 15:52:22 -0300

    Criação de operação de instanciação

--- src/main/java/com/weltonramos/cursomc/Application.java
+++ src/main/java/com/weltonramos/cursomc/Application.java
@@ -1,12 +1,32 @@
 package com.weltonramos.cursomc;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.weltonramos.cursomc.domain.Categoria;
+import com.weltonramos.cursomc.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class Application {
+public class Application implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
