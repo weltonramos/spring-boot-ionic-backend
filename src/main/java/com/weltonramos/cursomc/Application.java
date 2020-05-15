44b1688c0000c264f1d1904ca2b68fef5840b8c8
Welton Ramos <wel035@hotmail.com>
2020-05-15 16:02:03 -0300

    Criacao de pedido, pagamentos e repositorio

--- src/main/java/com/weltonramos/cursomc/Application.java
+++ src/main/java/com/weltonramos/cursomc/Application.java
@@ -1,5 +1,6 @@
 package com.weltonramos.cursomc;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.weltonramos.cursomc.domain.Cidade;
 import com.weltonramos.cursomc.domain.Cliente;
 import com.weltonramos.cursomc.domain.Endereco;
 import com.weltonramos.cursomc.domain.Estado;
+import com.weltonramos.cursomc.domain.Pagamento;
+import com.weltonramos.cursomc.domain.PagamentoComBoleto;
+import com.weltonramos.cursomc.domain.PagamentoComCartao;
+import com.weltonramos.cursomc.domain.Pedido;
 import com.weltonramos.cursomc.domain.Produto;
+import com.weltonramos.cursomc.domain.enums.EstadoPagamento;
 import com.weltonramos.cursomc.domain.enums.TipoCliente;
 import com.weltonramos.cursomc.repositories.CategoriaRepository;
 import com.weltonramos.cursomc.repositories.CidadeRepository;
 import com.weltonramos.cursomc.repositories.ClienteRepository;
 import com.weltonramos.cursomc.repositories.EnderecoRepository;
 import com.weltonramos.cursomc.repositories.EstadoRepository;
+import com.weltonramos.cursomc.repositories.PagamentoRepository;
+import com.weltonramos.cursomc.repositories.PedidoRepository;
 import com.weltonramos.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -29,18 +37,24 @@ public class Application implements CommandLineRunner {
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
+
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+
 	@Autowired
 	private CidadeRepository cidadeRepository;
-	
+
 	@Autowired
 	private ClienteRepository clienteRepository;
-	
+
 	@Autowired
 	private EnderecoRepository enderecoRepository;
+	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	
+	@Autowired
+	private PagamentoRepository pagamentoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -57,28 +71,43 @@ public class Application implements CommandLineRunner {
 
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
-		
+
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null, "São Paulo");
-		
+
 		Cidade c1 = new Cidade(null, "Uberlandia", est1);
 		Cidade c2 = new Cidade(null, "São Paulo", est2);
 		Cidade c3 = new Cidade(null, "Campinas", est2);
-		
+
 		est1.getCidades().addAll(Arrays.asList(c1));
 		est2.getCidades().addAll(Arrays.asList(c2, c3));
-		
+
 		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912777", TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
-		
+
 		Endereco e1 = new Endereco(null, "Rua flores", "300", "Apto 30", "Jardim", "38220834", cli1, c1);
 		Endereco e2 = new Endereco(null, "Av matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
-		
+
 		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		SimpleDateFormat simpleDate = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+
+		Pedido ped1 = new Pedido(null, simpleDate.parse("30/09/2017 10:37"), cli1, e1);
+		Pedido ped2 = new Pedido(null, simpleDate.parse("10/10/2017 19:35"), cli1, e2);
+
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2,
+				simpleDate.parse("20/10/2017 00:00"), null);
+
+		ped2.setPagamento(pagto2);
+
+		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
@@ -86,5 +115,7 @@ public class Application implements CommandLineRunner {
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
+		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
 	}
 }
