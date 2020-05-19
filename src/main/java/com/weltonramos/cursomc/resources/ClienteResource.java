4543c2e2955a93484333fca1a6ef995b8b6ff96b
Welton Ramos <wel035@hotmail.com>
2020-05-19 17:42:27 -0300

    GET, PUT e DELETE para cliente

--- src/main/java/com/weltonramos/cursomc/resources/ClienteResource.java
+++ src/main/java/com/weltonramos/cursomc/resources/ClienteResource.java
@@ -1,27 +1,70 @@
 package com.weltonramos.cursomc.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.weltonramos.cursomc.domain.Cliente;
+import com.weltonramos.cursomc.dto.ClienteDTO;
 import com.weltonramos.cursomc.services.ClienteService;
 
 @RestController
-@RequestMapping(value="/clientes")
+@RequestMapping(value = "/clientes")
 public class ClienteResource {
-	
+
 	@Autowired
 	private ClienteService clienteService;
-	
-	@RequestMapping(value="/{id}", method=RequestMethod.GET)
-	public ResponseEntity<Cliente> find(@PathVariable("id")Integer id) {
-		
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Cliente> find(@PathVariable("id") Integer id) {
+
 		Cliente cliente = clienteService.find(id);
-		
+
 		return ResponseEntity.ok(cliente);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO clienteDTO, @PathVariable("id") Integer id) {
+		Cliente cliente = clienteService.fromDTO(clienteDTO);
+		cliente.setId(id);
+		cliente = clienteService.update(cliente);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		clienteService.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+		List<Cliente> list = clienteService.findAll();
+		List<ClienteDTO> listDTO = list.stream().map(cliente -> new ClienteDTO(cliente))
+				.collect(Collectors.toList());
+		return ResponseEntity.ok(listDTO);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "24") Integer linesPerPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Cliente> list = clienteService.findPage(page, linesPerPage, orderBy, direction);
+		Page<ClienteDTO> listDto = list.map(cliente -> new ClienteDTO(cliente));
+
+		return ResponseEntity.ok(listDto);
 	}
 }
--- src/main/java/com/weltonramos/cursomc/services/ClienteService.java
+++ src/main/java/com/weltonramos/cursomc/services/ClienteService.java
@@ -1,12 +1,19 @@
 package com.weltonramos.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.weltonramos.cursomc.domain.Cliente;
+import com.weltonramos.cursomc.dto.ClienteDTO;
 import com.weltonramos.cursomc.repositories.ClienteRepository;
+import com.weltonramos.cursomc.service.exceptions.DataIntegrityException;
 import com.weltonramos.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -18,5 +25,39 @@ public class ClienteService {
 	public Cliente find(Integer id) {
 		Optional<Cliente> cliente= clienteRepository.findById(id);
 		return cliente.orElseThrow(() -> new ObjectNotFoundException("Id não encontrado" + Cliente.class.getName()));
+	}
+	
+	public Cliente update(Cliente cliente) {
+		Cliente newCliente = find(cliente.getId());
+		updateData(newCliente, cliente);
+		return clienteRepository.save(newCliente);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			clienteRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir clientes que possuam pedidos");
+		}
+	}
+	
+	public List<Cliente> findAll(){
+		return clienteRepository.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return clienteRepository.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO clienteDTO) {
+		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+
+	private void updateData(Cliente newCliente, Cliente cliente) {
+		newCliente.setNome(cliente.getNome());
+		newCliente.setEmail(cliente.getEmail());
 	}
 }
