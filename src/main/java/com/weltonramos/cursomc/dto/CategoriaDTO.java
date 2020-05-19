bfdb2426dd3e9a496e33bc46b58273de17712b3a
Welton Ramos <wel035@hotmail.com>
2020-05-19 12:59:16 -0300

    Validações sintatica com bean validation

--- src/main/java/com/weltonramos/cursomc/dto/CategoriaDTO.java
+++ src/main/java/com/weltonramos/cursomc/dto/CategoriaDTO.java
@@ -2,18 +2,25 @@ package com.weltonramos.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import com.weltonramos.cursomc.domain.Categoria;
 
-public class CategoriaDTO implements Serializable  {
-	
+public class CategoriaDTO implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
+
+	@NotEmpty(message = "Preenchimento obrigatório")
+	@Length(min = 5, max = 80, message = "Tamanho deve ser de 5 à 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
 	}
-	
+
 	public CategoriaDTO(Categoria categoria) {
 		id = categoria.getId();
 		nome = categoria.getNome();
@@ -27,11 +34,11 @@ public class CategoriaDTO implements Serializable  {
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getNome() {
 		return nome;
 	}
 
-	public void setName(String name) {
-		this.nome = name;
+	public void setNome(String nome) {
+		this.nome = nome;
 	}
 }
--- src/main/java/com/weltonramos/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/weltonramos/cursomc/resources/CategoriaResource.java
@@ -4,6 +4,8 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -35,15 +37,19 @@ public class CategoriaResource {
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@RequestBody Categoria categoria) {
+	public ResponseEntity<Void> insert(@Valid @RequestBody CategoriaDTO categoriaDTO) {
+		
+		Categoria categoria = categoriaService.fromDTO(categoriaDTO);
 		categoria = categoriaService.insert(categoria);
+		
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(categoria.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
 	}
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria categoria, @PathVariable("id") Integer id) {
+	public ResponseEntity<Void> update(@Valid @RequestBody CategoriaDTO categoriaDTO, @PathVariable("id") Integer id) {
+		Categoria categoria = categoriaService.fromDTO(categoriaDTO);
 		categoria.setId(id);
 		categoria = categoriaService.update(categoria);
 		return ResponseEntity.noContent().build();
